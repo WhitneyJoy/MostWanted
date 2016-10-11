6ede99074f78ea79ed0a75430cbfd565998ccc80
Whitney <bdwseybold@gmail.com>
2016-10-11 14:52:32 -0500

    downloaded testing files

---
+++ mwtesting/scripts-spec.js
@@ -0,0 +1,11 @@
+// describe('function addSixTo', function() {
+// 	it('add 6 to 2', function() {
+// 		expect(addSixTo(2)).toEqual(8);
+// 	});
+// });
+
+describe('function getInfo', function() {
+	it('pulls first name', function() {
+		expect(firstName().toEqual(Billy));
+	});
+});
